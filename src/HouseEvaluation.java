e857e0d304f638049aabc7ccadba121716a23010
frebern <my@email.com>
2016-12-07 20:31:13 +0900

    Oh my god

--- src/HouseEvaluation.java
+++ src/HouseEvaluation.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.OptionalDouble;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 
 public class HouseEvaluation {
 	
@@ -13,41 +9,32 @@ public class HouseEvaluation {
 	private String[] fields;
 	private ArrayList<String[]> tests;
 	
-	private ArrayList<Double> salePrices;
-	private HashMap<String,Double> avgBySection;
-	
 	public HouseEvaluation(){
 		
 		//1. 트레이닝/테스트/카테고리 데이터를 읽고 저장합니다. SalePrice는 따로 빼놓습니다.
 		readData();
-		saveSalePrices();
 		
-		attrDevide();
-		
-		//2. Numeric을 Category로 바꿔줍니다. SalePrice는 카테고리별 평균을 미리 계산해 놓습니다.
+		//2. Numeric을 Category로 바꿔줍니다.
 		convertData();
-		groupByAvg();
-		
-//		printData();
-		
-		long start = System.currentTimeMillis();
 		
 		//3. 어떤 알고리즘이든 상관없게끔 인터페이스 사용.
-		System.out.print("Now Running... ");
-		Algorithm algorithm = NaiveBayesian.getInstance();
-		//algorithm.readData(trains, tests, avgBySection);
-		algorithm.runAlgorithm();
-		System.out.println("Done!");
+		HashMap<Integer, Double> results = new HashMap<Integer, Double>();
+		tests.forEach(test->{
+			cnt=0;
+			int id = Integer.parseInt(test[0]);
+			double salePrice = recursion(trains,test,1.0);
+			results.put(id, salePrice);
+		});
 		
 		//4. 결과를 파일로 씁니다. output.svc에 써집니다.
-		//writeResult(algorithm.getProbabilities());
-		System.out.println("Time Elapsed: "+(System.currentTimeMillis()-start)/1000.0+"sec");
-		
+		writeResult(results);
 		
 	}
 	
+	int cnt=0;
 	// train을 절반씩 나누며 각각에 대한 확률을 구하고 recursive하게 수행한다.
 	private double recursion(ArrayList<String[]> trains_num, String[] test, double beforeProb){
+		System.out.println("Call #"+cnt++);
 		ArrayList<String[]> trains_upper = new ArrayList<>();
 		ArrayList<String[]> trains_lower = new ArrayList<>();
 		ArrayList<String[]> trains_cat = new ArrayList<>();
@@ -55,8 +42,8 @@ public class HouseEvaluation {
 		divide(trains_num, trains_cat, trains_upper, trains_lower);
 		
 		Algorithm algorithm = NaiveBayesian.getInstance();
-		//algorithm.readData(trains_cat, test);
-		//algorithms.runAlgorithm();
+		algorithm.readData(trains_cat, test);
+		algorithm.runAlgorithm();
 		upperProb = algorithm.getProbabilities().get("upper");
 		lowerProb = algorithm.getProbabilities().get("lower");
 		
@@ -131,33 +118,6 @@ public class HouseEvaluation {
 		});
 	}
 	
-	//뉴메릭컬한 SalePrice를 카테고리컬하게 변환하기 전에 미리 빼돌려놓습니다.
-	private ArrayList<Double> saveSalePrices(){
-		salePrices = trains.parallelStream()
-				   		   .map(train->Double.parseDouble(train[train.length-1]))
-				   		   .collect(Collectors.toCollection(ArrayList::new));
-		return salePrices;
-	}
-	
-	//범위별로 SalePrice 그룹을 나누고 각 그룹별 평균을 구합니다.
-	private HashMap<String,Double> groupByAvg(){
-		ArrayList<Predicate<Double>> ranges = DomainConvertor.getInstance().getDefinition("SalePrice").ranges;
-		avgBySection = new HashMap<>();
-		ranges.parallelStream()
-			  .map(range->salePrices.parallelStream()
-									.filter(range)
-									.mapToDouble(s->(double)s)
-									.average()
-									.getAsDouble()
-			  )
-			  .forEach(avg->{
-				  String key = DomainConvertor.getInstance().getCategory("SalePrice", avg+"");
-				  avgBySection.put(key, avg);
-			  });
-		
-		return avgBySection;
-	}
-	
 	private void convertData() {
 		DomainConvertor.getInstance().convert(fields, trains);
 		DomainConvertor.getInstance().convert(fields, tests);
@@ -217,14 +177,12 @@ public class HouseEvaluation {
 
 	//result<ID, SalePrice>
 	private void writeResult(HashMap<Integer, Double> result){
-		System.out.print("Now Writing... ");
 		final String filename = "output.csv";
 		Writer.getInstance().write(result, filename);
-		System.out.println("Done!");
 	}
 
 
-//	@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private void attrDevide(){
 		/***** Attribute Spliter *****/
 		String[] fields = Reader.getInstance().getFields();
@@ -249,7 +207,10 @@ public class HouseEvaluation {
 	
 
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 		new HouseEvaluation();
+		System.out.println("Done!");
+		System.out.println("Time Elapsed: "+(System.currentTimeMillis()-start)/1000.0+"sec");
 	}
 	
 }
